7a9768b562e06b3b67f6eb843ede8b288504d0b4
India Rex <face940@gmail.com>
2020-07-14 15:10:13 +0100

    add not null to sql set up info, delete unnecessary tests, first user story complete

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -6,13 +6,4 @@ feature 'sign-up' do
     click_button('Sign Up')
     expect(page).to have_content("Congratulations! You are part of the mob!")
   end
-
-  scenario 'The administrator can see the users details on the /users page' do
-    visit ('/')
-    fill_in('email_sign_up', with: 'mobtheworld@mob.com')
-    fill_in('password_sign_up', with: 'hello1')
-    click_button('Sign Up')
-    expect('/users').to have_content("mobtheworld@mob.com")
-    expect('/users').to have_content("hello1")
-  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,19 +1,4 @@
 require './lib/user'
 
 describe User do
-  subject(:user) {User.new}
-  describe "#add_email" do
-    it "allows user to add email" do
-      subject.add_email("mobtheworld@mob.com")
-      expect(subject.email).to eq("mobtheworld@mob.com")
-    end
-  end
-  describe "#add_password" do
-    it "allows user to add pw" do
-      subject.add_password("hello1")
-      expect(subject.password).to eq("hello1")
-    end
-  end
-
 end
-
