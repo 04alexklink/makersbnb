103b5de9da7f2a9462594d9c09eb550c2a0bb8ea
Alessa <alessa_hardwick@live.co.uk>
2020-07-14 11:14:15 +0100

    a user is able to input email and password

--- app.rb
+++ app.rb
@@ -4,9 +4,14 @@ require './lib/mobbnb'
 class Mob_Boss < Sinatra::Base
 
   get '/' do
-    erb :index  
+    erb :index
   end
-  
+
+  post '/signup' do
+    p params
+    erb :signup
+  end
+
 
   run! if app_file == $0
 end
