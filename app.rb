064806e11acf95ce29a1535d388021755d242f5c
Josh <joshua.ng2332@gmail.com>
2020-07-14 13:08:18 +0100

    The administrator can see the users details on the /users page. Still failing.

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ class Mob_Boss < Sinatra::Base
     user = User.new
     user.add_email(email: params[:email_sign_up])
     user.add_password(password: params[:password_sign_up])
+    User.create(email: params[:email_sign_up], password: params[:password_sign_up])
     erb :signup
+  end
+
+  get '/users' do
+    @users = User.all
+    erb :users
   end
 
 
