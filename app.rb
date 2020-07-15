5aad1740f508dbbc40757c29805706c0ee3ab64b
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-15 11:42:55 +0100

    Frontend log in functionality

--- app.rb
+++ app.rb
@@ -2,10 +2,30 @@ require 'sinatra/base'
 require './lib/user'
 
 class Mob_Boss < Sinatra::Base
-  # enable :sessions
+  enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/logincheck' do
+    session[:email_log_in] = params[:email_log_in]
+    session[:password_log_in] = params[:password_log_in]
+    p session
+  # User.authenticate(params[:email_log_in], params[:password_log_in)
+  # NOT GOING TO USE user.authetnicate here, but in get logincheck route
+    redirect '/logincheck'
+  end
+
+  get '/logincheck' do
+   # @result = User.authenticate(session[:email_log_in], session[:password_log_in])
+    @result = true
+   erb :logincheck
+
   end
 
   post '/signup' do
