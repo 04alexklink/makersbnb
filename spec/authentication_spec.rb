1830501895a377eb555b725e3d3d15f330a73c7e
Josh <joshua.ng2332@gmail.com>
2020-07-15 11:23:55 +0100

    Coverage added via simplecov.

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -1,7 +1,7 @@
 describe 'authentication' do
   
   it 'It returns true if the email and password match' do
-    user = User.create(email: 'mobtheworld@mob.com', password: 'hello1')
+    User.create(email: 'mobtheworld@mob.com', password: 'hello1')
     user2 = User.new(email: 'mobtheworld@mob.com', password: 'hello1')
     #user2.authenticate
     expect(user2.authenticate).to be(true)
